0447a7e40dbe0bd022aae851078d29abae5a8992
Srivatsav, Gunisetty <srivatsav.gunisetty@philips.com>
2020-09-16 16:55:56 +0530

    Add certificate exchange step

--- client/client.cpp
+++ client/client.cpp
@@ -21,39 +21,46 @@ void initWinSock()
 	printf("Initialised\n");
 }
 
-int main() 
-{ 
-	int sock = 0; 
-	struct sockaddr_in serv_addr; 
-	const char *hello = "Hello from client"; 
-	char buffer[1024] = {0}; 
+int main()
+{
+	int sock = 0;
+	struct sockaddr_in serv_addr;
+	const char *hello = "Hello from client";
+	char buffer[1024] = { 0 };
 
 	initWinSock();
 
-	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
-	{ 
-		printf("\n Socket creation error \n"); 
-		return -1; 
-	} 
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		printf("\n Socket creation error \n");
+		return -1;
+	}
 
-	serv_addr.sin_family = AF_INET; 
-	serv_addr.sin_port = htons(PORT); 
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(PORT);
 
 	// Convert IPv4 and IPv6 addresses from text to binary form 
-	if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) 
-	{ 
-		printf("\nInvalid address/ Address not supported \n"); 
-		return -1; 
-	} 
+	if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
+	{
+		printf("\nInvalid address/ Address not supported \n");
+		return -1;
+	}
 	//Connect to server
-	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
-	{ 
-		printf("\nConnection Failed \n"); 
-		return -1; 
-	} 
-	send(sock , hello , strlen(hello) , 0 ); 
-	printf("Hello message sent\n"); 
-	recv( sock , buffer, 1024, 0); 
-	printf("Received: %s\n",buffer ); 
-	return 0; 
-} 
+	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+	{
+		printf("\nConnection Failed \n");
+		return -1;
+	}
+
+	printf("Waiting for certificate!\n");
+	recv(sock, buffer, 1024, 0);
+	printf("Received Certificate: %s\n", buffer);
+
+	printf("Sending message!\n");
+	send(sock, hello, strlen(hello), 0);
+	printf("Hello message sent\n");
+
+	recv(sock, buffer, 1024, 0);
+	printf("Received: %s\n", buffer);
+	return 0;
+}
--- server/server.cpp
+++ server/server.cpp
@@ -1,11 +1,24 @@
 #include <stdio.h> 
 #include <stdlib.h> 
-#include <string.h> 
-
+#include <string> 
+#include <iostream>
 
 #include <winsock2.h>
 
 #pragma comment(lib,"ws2_32.lib") //Winsock Library
+
+class Certificate
+{
+private:
+	std::string name;
+	unsigned int hash;
+public:
+	Certificate(const std::string& n, int val) : name(n), hash(val) {}
+	std::string toString() {
+		std::string response = name + ";" + std::to_string(hash);
+		return response;
+	}
+};
 
 const int PORT = 8080;
 
@@ -22,52 +35,61 @@ void initWinSock()
 	printf("Initialised\n");
 }
 
-int main() 
-{ 
+int main()
+{
 	int server_fd = 0;
 	int new_socket = 0;
-	struct sockaddr_in address; 
-	int addrlen = sizeof(address); 
-	char buffer[1024] = {0}; 
-	const char *hello = "Hello from server"; 
-	
+	struct sockaddr_in address;
+	int addrlen = sizeof(address);
+	char buffer[1024] = { 0 };
+	const char *hello = "Hello from server";
+
+	Certificate c("www.localhost.com", 1234);
+	std::string certificateSerialized = c.toString();
+
 	initWinSock();
 
 	// Create socket descriptor 
-	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
-	{ 
-		perror("socket failed"); 
-		exit(EXIT_FAILURE); 
-	} 
-	
-	address.sin_family = AF_INET; 
-	address.sin_addr.s_addr = INADDR_ANY; 
-	address.sin_port = htons( PORT ); 
-	
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+	{
+		perror("socket failed");
+		exit(EXIT_FAILURE);
+	}
+
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons(PORT);
+
 	// Attach socket to the port 8080 
-	if (bind(server_fd, (struct sockaddr *)&address, 
-								sizeof(address))<0) 
-	{ 
-		perror("bind failed"); 
-		exit(EXIT_FAILURE); 
-	} 
+	if (bind(server_fd, (struct sockaddr *)&address,
+		sizeof(address)) < 0)
+	{
+		perror("bind failed");
+		exit(EXIT_FAILURE);
+	}
 	// Setup listen queue
-	if (listen(server_fd, 3) < 0) 
-	{ 
-		perror("listen"); 
-		exit(EXIT_FAILURE); 
-	} 
+	if (listen(server_fd, 3) < 0)
+	{
+		perror("listen");
+		exit(EXIT_FAILURE);
+	}
 	printf("Waiting for connection...\n");
-	if ((new_socket = accept(server_fd, (struct sockaddr *)&address, 
-								&addrlen))<0) 
-	{ 
-		perror("accept"); 
-		exit(EXIT_FAILURE); 
-	} 
+	if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+		&addrlen)) < 0)
+	{
+		perror("accept");
+		exit(EXIT_FAILURE);
+	}
+
+	printf("Sending certificate information\n");
+	send(new_socket, certificateSerialized.c_str(), sizeof(certificateSerialized), 0);
+
 	printf("Waiting for data...\n");
-	recv( new_socket , buffer, 1024, 0); 
-	printf("Received: %s\n",buffer ); 
-	send(new_socket , hello , strlen(hello) , 0 ); 
-	printf("Hello message sent\n"); 
-	return 0; 
-} 
+	recv(new_socket, buffer, 1024, 0);
+	printf("Received: %s\n", buffer);
+
+	send(new_socket, hello, strlen(hello), 0);
+	printf("Hello message sent\n");
+
+	return 0;
+}
